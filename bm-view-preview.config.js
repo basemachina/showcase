a496b142d71adc34997a11cf49c0705f51786ae5
timakin <timaki.st@gmail.com>
2024-07-31 20:10:59 +0900

    fix: ignore

--- bm-view-preview.config.js
+++ bm-view-preview.config.js
@@ -1,9 +1,9 @@
 // 下記の変数に、ビューのプレビューに使用する企業アカウントのサブドメイン、プロジェクトID、環境IDを設定してください
-const subdomain = "<subdomain>";
-const projectId = "<projectId>";
-const environmentId = "<environmentId>";
+const subdomain = "basemachina";
+const projectId = "cpil1li9io6g00fcf4sg";
+const environmentId = "cpil1li9io6g00fcf4t0";
 
 export default {
-  baseUrl: `https://${subdomain}.basemachina.com/projects/${projectId}/environments/${environmentId}`,
+  baseUrl: `https://${subdomain}.basemachina.dev/projects/${projectId}/environments/${environmentId}`,
   sourceDir: "./dist",
 };
