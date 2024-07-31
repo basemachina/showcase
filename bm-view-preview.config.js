a306e31dc88acaa0995a257edb4af59dfc958c21
timakin <timaki.st@gmail.com>
2024-07-31 20:11:21 +0900

    .gitignore is now working

--- bm-view-preview.config.js
+++
@@ -1,9 +0,0 @@
-// 下記の変数に、ビューのプレビューに使用する企業アカウントのサブドメイン、プロジェクトID、環境IDを設定してください
-const subdomain = "basemachina";
-const projectId = "cpil1li9io6g00fcf4sg";
-const environmentId = "cpil1li9io6g00fcf4t0";
-
-export default {
-  baseUrl: `https://${subdomain}.basemachina.dev/projects/${projectId}/environments/${environmentId}`,
-  sourceDir: "./dist",
-};
